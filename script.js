ef9f4d9f212b6226eca6c9430a165a607955260f
Christian Colon <ccolon100@gmail.com>
2023-01-06 01:31:39 +0700

    Final Version

--- script.js
+++ script.js
@@ -17,32 +17,92 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
+const mainTitle = document.getElementById('main-title');
+mainTitle.textContent = 'Welcome To My Personal Website';
+
+// document.querySelector('#main-title').textContent = 'Welcome to My Personal Website';
 
 
   // Part 2
+const body = document.querySelector('body');
+body.style.backgroundColor = 'pink';
 
+// document.querySelector('body').style.backgroundColor = 'blue';
 
   // Part 3
+const favoriteThingsList = document.querySelector('#favorite-things');
+const lastListItem = favoriteThingsList.lastElementChild;
+favoriteThingsList.removeChild(lastListItem);
 
+// const favoriteThingsList = document.querySelector('#favorite-things > li')
+
+// document.querySelector('#favorite-things').removeChild(favoriteThingsList[favoriteThingsList.length - 1]);
 
   // Part 4
+  const specialTitles = document.querySelectorAll('.special-title');
+  for (let i = 0; i < specialTitles.length; i++) {
+  specialTitles[i].style.fontSize = '2rem';
+  }
 
+  // document.querySelectorAll('.special-title').forEach((item) => {
+  //   item.style.fontSize = '2rem';
+  // })
 
   // Part 5
+  var newRaces = document.getElementById('past-races');
+  newRaces.removeChild(newRaces.getElementsByTagName('li')[3]);
 
-
+  // document.querySelectorAll('#past-races > li').forEach((item) => {
+  //   if (item.textContent.trim() === 'Chicago') {
+  //     document.querySelector('#past-races').removeChild(item);
+  //   }
+  // });
+  
+  
   // Part 6
+  const pastRacesList2 = document.querySelector('#past-races');
+  const newListItem = document.createElement('li');
+  newListItem.textContent = 'Bangkok';
+  pastRacesList2.appendChild(newListItem);
 
+  // const li = document.createElement('li')
+  // li.textContent = 'cincinnati'
+  // document.querySelector('#past-races').appendChild(li);
+  
 
   // Part 7
 
+  
+  
+ const blogPost = document.createElement('div');
+ blogPost.classList.add('blog-post');
+ blogPost.classList.add('purple');
+ const heading = document.createElement('h1')
+ heading.textContent = 'Bangkok'
+ const pE1 = document.createElement('p')
+ pE1.textContent = 'Bangkok is the best city to drive fast cars!'
+ document.querySelector('.main').appendChild(blogPost);
+ blogPost.appendChild(heading);
+ blogPost.appendChild(pE1);
+
+
 
   // Part 8
+  const quoteTitle = document.querySelector('#quote-title');
+  quoteTitle.addEventListener('click', randomQuote);
 
+  document.querySelector('#quote-title').addEventListener('click', () => randomQuote());
 
   // Part 9
+  const blogPosts = document.querySelectorAll('.blog-post');
 
-
-
-
+  for (let i = 0; i < blogPosts.length; i++) {
+    const blogPost = blogPosts[i];
+    blogPost.addEventListener('mouseout', function() {
+      blogPost.classList.toggle('purple');
+    });
+    blogPost.addEventListener('mouseenter', function() {
+      blogPost.classList.toggle('red');
+    });
+  }
 });
